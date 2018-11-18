63b84a988cd2a0704f49964a79736f1c51b9c950
acazuc <acazuc@student.42.fr>
2018-11-18 20:25:54 +0100

    Added write on change

--- src/nouveau-indicator.c
+++ src/nouveau-indicator.c
@@ -1,14 +1,28 @@
 #include <libappindicator/app-indicator.h>
 #include <gtk/gtk.h>
+#include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
 
-//#define FILENAME "/sys/kernel/debug/dri/0/pstate"
-#define FILENAME "pstate_test"
+#define FILENAME "/sys/kernel/debug/dri/0/pstate"
+//#define FILENAME "pstate_test"
 
 static void menu_item_callback(GtkWidget *widget, char *data)
 {
+	(void)widget;
+	(void)data;
+	int pstate_file = open(FILENAME, O_WRONLY | O_TRUNC);
+	if (pstate_file == -1)
+		return;
+	ssize_t osef = write(pstate_file, data, strlen(data));
+	(void)osef;
+	close(pstate_file);
+}
+
+static void menu_item_exit_callback(GtkWidget *widget, char *data)
+{
+	exit(EXIT_SUCCESS);
 	(void)widget;
 	(void)data;
 }
@@ -51,6 +65,10 @@ int main(int argc, char **argv)
 		newline = tmp + 1;
 	}
 display:
+	;
+	GtkWidget *indicator_menu_item = gtk_menu_item_new_with_label("Exit");
+	g_signal_connect(indicator_menu_item, "activate", G_CALLBACK(menu_item_exit_callback), NULL);
+	gtk_menu_append(GTK_MENU(indicator_menu), indicator_menu_item);
 	gtk_widget_show_all(indicator_menu);
 	app_indicator_set_status(indicator, APP_INDICATOR_STATUS_ACTIVE);
 	app_indicator_set_menu(indicator, GTK_MENU(indicator_menu));
